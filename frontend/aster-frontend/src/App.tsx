e09b20f98f196826c86ad09c042f738c60d68471
Guilherme Aika Alves Varella <guilherme.avarella@gmail.com>
2025-10-29 15:11:05 -0300

    chore: more dependencies fixes, updates n stuff

--- frontend/aster-frontend/src/App.tsx
+++ frontend/aster-frontend/src/App.tsx
@@ -1,12 +1,11 @@
 import { useState } from 'react'
-import logo from '/public/favicon.svg'
 import LiquidGlass from 'liquid-glass-react'
 
 
-function App() {
+function App() {  
   return (
-    <div className="min-h-screen min-w-screen flex flex-row items-start justify-start bg-background-aurora">
-      <section className="m-10 p-10 rounded-lg text-white bg-black"> Teste </section>
+    <div className="min-h-screen min-w-screen flex flex-row items-start justify-start">  
+      <section className="m-10 p-10 rounded-lg text-white bg-content-primary"> Teste </section>
       <section> Teste </section>
     </div>
   )
