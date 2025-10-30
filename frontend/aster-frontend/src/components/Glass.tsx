aacde56ad5a7d6e36ebec207a0593d8f7945ce29
Guilherme Aika Alves Varella <guilherme.avarella@gmail.com>
2025-10-30 13:57:49 -0300

    feat[component]: Glass

---
+++ frontend/aster-frontend/src/components/Glass.tsx
@@ -0,0 +1,24 @@
+import { LiquidGlassReact } from 'solid-glass/react';
+import "solid-glass/style.css";
+
+// Para padding personalizado, use <Glass padding="p-X"> ...
+
+export default function Glass({ padding = 'p-6', children }: { padding?: string, children: React.ReactNode }) {
+  return (
+    <LiquidGlassReact
+        width="fit-content"
+        height="fit-content"
+        shadowColor="rgba(255, 255, 255, 0.5)"
+        shadowBlur={0.75}
+        shadowSpread={0.45}
+        frostBlur={30}
+        borderRadius={48}
+        className={`min-h-[calc(100vh-3rem)] min-w-80 max-h-268 ${padding} shadow-lg rounded-[48px] border border-[var(--background-fixed-white)]/60 bg-[var(--background-fixed-white)]/40 pointer-events-none select-none`}
+        style={{ backdropFilter: 'blur(10px)' }}
+    >
+        <div className={`pointer-events-auto`}>
+        {children}
+        </div>
+    </LiquidGlassReact>
+  );
+}
