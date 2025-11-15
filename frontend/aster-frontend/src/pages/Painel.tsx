555703fda06c2a33024981440d05c28ff1cf7d80
Guilherme Aika Alves Varella <guilherme.avarella@gmail.com>
2025-11-15 13:51:02 -0300

    fix: tech nested route

--- frontend/aster-frontend/src/pages/Painel.tsx
+++ frontend/aster-frontend/src/pages/Painel.tsx
@@ -1,8 +1,8 @@
 import Estrategia_D from "./Painel/Estrategia_D"
-import Estrategia_I from "./Painel/Estrategia_I"
 import Financas_D from "./Painel/Financas_D"
-import Financas_I from "./Painel/Financas_I"
 import Tech_D from "./Painel/Tech_D"
+import Estrategia_I from "./Painel/Estrategia_I"
+import Financas_I from "./Painel/Financas_I"
 import Tech_I from "./Painel/Tech_I"
 
 export const painelRoutes = [
@@ -11,11 +11,11 @@ export const painelRoutes = [
     children: [
       { path: "d/estrategia", element: <Estrategia_D /> },
       { path: "d/financas", element: <Financas_D /> },
-      { path: "d/tech", element: <Tech_D /> },
-
+      { path: "d/techlead", element: <Tech_D /> },
+      
       { path: "i/estrategia", element: <Estrategia_I /> },
       { path: "i/financas", element: <Financas_I /> },
-      { path: "i/tech", element: <Tech_I /> }
+      { path: "i/techlead", element: <Tech_I /> }
     ]
   }
 ];
