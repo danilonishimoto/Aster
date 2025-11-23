0930a3c0006f07d28639f63cf11945d6027fb4a1
danilonishimoto <daniyuzo.nishimoto@gmail.com>
2025-11-23 18:35:09 -0300

    feat: add Contem

---
+++ backend/aster-dashboard-backend/src/main/java/com/aster/aster_dashboard_backend/controller/api/operacoes/AdquireController.java
@@ -0,0 +1,14 @@
+package com.aster.aster_dashboard_backend.controller.api.operacoes;
+
+import com.aster.aster_dashboard_backend.dto.DevolutivaFeedbackDto;
+import com.aster.aster_dashboard_backend.dto.PageResponseDto;
+import com.aster.aster_dashboard_backend.service.DevolutivaFeedbackService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/operacoes/adquire")
+public class AdquireController {
+    
+}
---
+++ backend/aster-dashboard-backend/src/main/java/com/aster/aster_dashboard_backend/dto/UsaDto.java
@@ -0,0 +1,22 @@
+package com.aster.aster_dashboard_backend.dto;
+
+import java.util.List;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+
+@Getter
+@Setter
+@SuperBuilder
+@NoArgsConstructor
+@AllArgsConstructor
+public class UsaDto {
+    
+    private List<String> usuariosChaveUso;
+    private String licencaId;
+    private String clienteDocumento;
+
+}
