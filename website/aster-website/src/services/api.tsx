0f498824fb8cf6476032512877de2cb7f5154dfd
danilonishimoto <44167818+danilonishimoto@users.noreply.github.com>
2025-11-27 10:02:07 -0300

    Update api.tsx

--- website/aster-website/src/services/api.tsx
+++ website/aster-website/src/services/api.tsx
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({ 
-  baseURL: 'http://aster-backend-env.eba-yqmhtgkn.sa-east-1.elasticbeanstalk.com/'
+  baseURL: 'https://aster-backend-env.eba-yqmhtgkn.sa-east-1.elasticbeanstalk.com/'
 })
 
 export default api
